49d7fb0998679d6426fef2b2f4b72f9607376e37
magnus_manske <magnus_manske@16f74cb3-8e16-0410-8422-8e7467ed96e7>
2005-03-01 12:14:11 +0000

    AutoAnnotate

--- trunk/AutoAnnotate.h
+++ trunk/AutoAnnotate.h
@@ -9,17 +9,24 @@ class AutoAnnotate
 	{
 	public :
 	AutoAnnotate ( MyChild *_p = NULL ) ;
-	virtual void ScanDatabase ( wxString database ) ;
+	virtual bool SettingsDialog () ;
+	virtual void Run () ;
 	
 	private :
+	virtual bool ScanDatabase ( wxString database ) ;
 	virtual bool addORFs ( TVector *v ) ;
  	virtual bool MatchItem ( TVector *tv , TVectorItem &item , TVector *v , wxString &oseq ) ;
  	virtual bool RawMatch ( TVectorItem &item , TVector *v , wxString &oseq , wxString &s ) ;
  	virtual void machete ( TVector *v ) ;
  	virtual bool within ( TVectorItem &i1 , TVectorItem &i2 , TVector *v ) ;
+
+ 	friend class TAutoAnnotateDialog ;
  	
  	MyChild *p ;
  	wxArrayString alreadyin ;
+ 	wxString commonVectorsDatabase , additionalDatabase ;
+ 	bool useCommonDatabase , useAdditionalDatabase , useMachete , doAddORFs ;
+    wxArrayString dbname , dbfile ;
 	} ;    
 
 #endif
