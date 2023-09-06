73ee8730c048affcdf138506215ce2407356ae57
Steffen Moeller <moeller@debian.org>
2023-09-06 11:00:39 +0200

    configure.ac: extend init, -I$PREFIX/include

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-export WANT_AUTOCONF_2_5="1"
-export WANT_AUTOMAKE_1_7="1"
+#export WANT_AUTOCONF_2_5="1"
+#export WANT_AUTOMAKE_1_7="1"
 
 aclocal
 libtoolize --automake --force --copy
 automake -a -c
 autoconf
-
